1f19d2b5f174da44cb918bda3cb89d73fe8fad7b
arifuxd <arifofficial00@gmail.com>
2021-08-15 23:26:57 +0600

    project done

--- src/Todo.js
+++ src/Todo.js
@@ -1,34 +1,38 @@
-import { ListItem, ListItemText, Button, Modal } from '@material-ui/core';
+import { ListItem, ListItemText, Button, Modal, FormControl, Input, InputLabel } from '@material-ui/core';
 import React from 'react';
 import { db } from './firebase';
 import './Todo.css'
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever'
 import { useState } from 'react';
+
+
 const Todo = ({todo}) => {
-    const [open, setOpen] = useState(false);
-    const handleOpen = ()=>{
-        setOpen(true)
+
+    const [input, setInput] = useState('')
+    const handleClick = () =>{
+        db.collection('todos').doc(todo.id).set({
+            todo : input
+        }, 
+        {merge : true}
+        )
     }
-   
     return (
             <>
-            <Modal 
-            open={open}
-            onClose = {(e) => setOpen(false)}
-            aria-labelledby="simple-modal-title"
-            aria-describedby="simple-modal-description"
-            >
-                <div>
-                    <h1>I am a Modal</h1>
-                    <button onCLick={()=> setOpen(false)}>Close</button>
-                </div>
-            </Modal>
+       
             <ListItem>
                 <ListItemText primary={todo.todo} secondary={todo.secondary}>
                 </ListItemText>
+                <FormControl>
+        <Input id="my-input" placeholder={todo.todo} type="text" value={input} onChange={(e)=> setInput(e.target.value)} />
+        <Button onClick={handleClick} variant="outlined">Update</Button>
+                
+    </FormControl>
                 <Button onClick={e => db.collection('todos').doc(todo.id).delete()} variant="contained" color="secondary"><DeleteForeverIcon/> &nbsp;DELETE ME</Button>
+               
+
+
             </ListItem>
-            <Button onClick={e => setOpen(true)}>Edit</Button>
+           
             </>
     );
 };
